555c1bc9f8bf9258e2f938bfe5fe912e03b85adb
Fish <thehuiq2006@gmail.com>
2026-03-07 07:56:07 -0800

    basic window

---
+++ Makefile
@@ -0,0 +1,2 @@
+game: main_project.c
+	g++ main_project.c -o game -lSDL2
--- main_project.c
+++ main_project.c
@@ -1,1 +1,71 @@
-test
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define WIDTH 400
+#define HEIGHT 400
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	(void) argv;
+
+	//Setup
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Error initalizing SDL: " << SDL_GetError() << '\n';
+		return 1;
+	}
+	
+	SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
+	window = SDL_CreateWindow
+	(
+		"Pet",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP
+	);
+
+	if(!window)
+	{
+		std::cout << "Error Creating Window: " << SDL_GetError() << '\n';
+		return 1;
+	}
+	
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(!renderer)
+	{
+		std::cout << "Error Getting Renderer: " << SDL_GetError() << '\n';
+		return 1;
+	}
+
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	//DesktopPet
+	bool isRunning = true;
+	SDL_Event event;
+
+	while(isRunning)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+			{
+				isRunning = false;
+			}	
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+
+		
+		//Game Logic
+	}
+
+	SDL_Quit();
+	return 0;
+}
